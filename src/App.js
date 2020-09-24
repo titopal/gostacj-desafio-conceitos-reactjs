4394b628a485fc969ecc881de8f0ebf8e72951c1
tito <titopalcorrea@gmail.com>
2020-09-24 16:42:09 -0300

    App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
-import "./styles.css";
+import './styles.css';
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'Umbriel',
+      url: 'http://titopal.com',
+      techs: ['node', 'react'],
+    });
+
+    setRepositories([...repositories, response.data]); // principio de imutabilidade, precisa copiar os repositorios e adicionar novo
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
-  }
+    await api.delete(`repositories/${id}`);
 
+    const newRepositories = repositories.filter(
+      (repository) => repository.id !== id // mantendo apenas nos repositórios aonde o id é diferente do id que removeu
+    );
+
+    setRepositories(newRepositories);
+  }
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
